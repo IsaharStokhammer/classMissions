13665c84153afe0d03f8f97afb48ba26be40862b
IsaharStokhammer <8526656@gmail.com>
2024-09-08 12:22:55 +0300

    1

--- 08-09-2024-2/script.js
+++ 08-09-2024-2/script.js
@@ -1,3 +1,17 @@
-function revNumber(...nums) {
-    return nums.reverse();
+function mission1(num, dir) {
+    let str = "";
+    if (dir) {
+        for (let i = 0; i < num; i++) {
+            str += "-";
+        }
+        str += ">";
+    } else {
+        str = "<";
+        for (let i = 0; i < num; i++) {
+            str += "-";
+        }
+    }
+    console.log(str);
 }
+
+
