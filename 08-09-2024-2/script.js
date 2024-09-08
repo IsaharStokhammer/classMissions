f4b8822ce8967152bdd726c391dc2e6dc76ef98f
IsaharStokhammer <8526656@gmail.com>
2024-09-08 12:40:34 +0300

    שאלה 2 הושלמה

--- 08-09-2024-2/script.js
+++ 08-09-2024-2/script.js
@@ -13,5 +13,36 @@ function mission1(num, dir) {
     }
     console.log(str);
 }
+mission1(3, true);
 
+function findMax(numArr) {
+    let max = numArr[0];
+    for (let i = 0; i < numArr.length; i++) {
+        if (numArr[i] > max) {
+            max = numArr[i];
+        }
+    }
+    return max;
+}
+function mission2() {
+    let numArr =[];
+    while (true) {
+        let inputNum = Number(prompt("הכנס מספר"));
+        numArr.push(inputNum);
+        if (inputNum === 0) {
+            console.log("המספר הגבוה ביותר הוא " + findMax(numArr));
+            console.log("מספרים נוספו: " + numArr.length);
+            console.log("סך כל המספרים: " + numArr.reduce((a, b) => a + b, 0));
+            console.log("ממוצע המספרים הוא: " + numArr.reduce((a, b) => a + b, 0) / numArr.length);
+            if (numArr.length >= 4) {
+                console.log("המספר הרביעי שנקלט הוא: " + numArr[4]);                
+            }
+            else {
+                console.log(
+                "לא נמצא מספר רביעי, המספר החרון שנקלט הוא: " + numArr[numArr.length - 1]
+                );}
+            break;
+        }
+    }
+}
 
