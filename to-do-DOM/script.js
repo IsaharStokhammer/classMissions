17871ff576435942ab38a8ba7f768d072897abdf
IsaharStokhammer <8526656@gmail.com>
2024-09-09 18:01:35 +0300

    kuygkjh

--- to-do-DOM/script.js
+++ to-do-DOM/script.js
@@ -1,13 +1,22 @@
 const input = document.getElementById("input");
 const addButton = document.getElementById("addButton");
-const table = document.getElementById("table");
-const rows = table.rows;
-const toDos = JSON.parse(localStorage.getItem('toDos'));
-let idCounter = toDos.length + 1;
+const tableContainer = document.getElementById("tableContainer");
+const rows = tableContainer.rows;
+let toDos = localStorage.getItem('toDos');
+const table = document.querySelector("table");
+// const sortTodosByName = getElementById("sortByName");
+
+if (!toDos) {
+    toDos = [];
+} else {
+    toDos = JSON.parse(toDos);
+}
 render();
+
 addButton.addEventListener("click", () => {
+
     const toDo = {
-        id: idCounter,
+        id: Date.now().toString() + Math.random().toString() + 1,
         text: input.value,
         status: false
     };
@@ -16,21 +25,22 @@ addButton.addEventListener("click", () => {
     }
     input.value = "";
     input.innerText = "";
-    idCounter ++;
     render();
 });
 function render() {
+
     table.innerHTML = "";
-    toDos.forEach((toDo, index) => {    
+    toDos.forEach((toDo, index) => {
         const row = document.createElement("tr");
         const id = document.createElement("td");
         const text = document.createElement("td");
         const status = document.createElement("td");
         const actions = document.createElement("td");
 
-        id.innerText = toDo.id;
+        id.innerText = toDo.id.toString().substring(0, 3) + "...";
         text.innerText = toDo.text;
         status.innerText = toDo.status ? "done" : "in process";
+
         const editButton = document.createElement("button");
         const deleteButton = document.createElement("button");
         const changeStatusButton = document.createElement("button");
@@ -38,9 +48,9 @@ function render() {
         editButton.innerText = "edit";
         editButton.addEventListener("click", editByIndex(index));
         deleteButton.innerText = "delete";
-        deleteButton.addEventListener("click", deleteByIndex(index));
+        deleteButton.addEventListener("click", ()=> deleteByIndex(index));
         changeStatusButton.innerText = "change status";
-        changeStatusButton.addEventListener("click", changeStatusByIndex(index));   
+        changeStatusButton.addEventListener("click",()=> changeStatusByIndex(index));
 
         actions.appendChild(editButton);
         actions.appendChild(deleteButton);
@@ -50,16 +60,24 @@ function render() {
         row.appendChild(text);
         row.appendChild(status);
         row.appendChild(actions);
-
-        table.appendChild(row);
+        table.appendChild(row)
+        tableContainer.appendChild(table);
         localStorage.setItem('toDos', JSON.stringify(toDos));
 
     });
 }
-function editByIndex(index) {}
+function editByIndex(index) { }
 function deleteByIndex(index) {
-
+    toDos.splice(index, 1);
+    localStorage.setItem('toDos', JSON.stringify(toDos));
+    render();
 }
 function changeStatusByIndex(index) {
-
+    if (toDos[index].status == false) { toDos[index].status = true } else { toDos[index].status = false; }
+    localStorage.setItem('toDos', JSON.stringify(toDos));
+    render();
 }
+// sortTodosByName.addEventListener("click", sortTodosByName(index));
+// function sortTodosByName() {
+//     toDos.sort((a, b) => (a.text > b.text) ? 1 : -1);
+// }
